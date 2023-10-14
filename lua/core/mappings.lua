f186cefb1c5bf7052c36cf305d39b9526f3103d3
RomaS <romaha2901@gmail.com>
2023-10-14 22:07:38 +0300

    Window mappings
    Change window size mappings add

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -78,6 +78,10 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<c-Left>", ":wincmd <<CR>")
+vim.keymap.set("n", "<c-Up>", ":wincmd +<CR>")
+vim.keymap.set("n", "<c-Right>", ":wincmd ><CR>")
+vim.keymap.set("n", "<c-Down>", ":wincmd -<CR>")
 
 -- Terminal
 vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
